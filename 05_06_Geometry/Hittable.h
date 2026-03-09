6dcde4c570c7218c60eb54907fa8f1be38b683f3
akrami <ramerame123890@gmail.com>
2026-03-09 20:02:02 +0200

    add front/back face detection support

--- 05_06_Geometry/Hittable.h
+++ 05_06_Geometry/Hittable.h
@@ -6,6 +6,16 @@ struct HitRecord{
     Point3 p;
     Vec3 normal;
     Float t;
+    bool front_face;
+
+    void set_face_normal(const Ray& ray, const Vec3& outward_normal){
+        // Sets the hit record normal vector.
+        // NOTE: the parameter `outward_normal` is assumed to have unit length.
+        
+        front_face = dot(ray.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal : -outward_normal;
+    }
+
 };
 
 class Hittable{
