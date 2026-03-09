d99bf45525a6236548b82788e825115f7aa72e12
akrami <ramerame123890@gmail.com>
2026-03-09 20:15:39 +0200

    use ZERO constatns instead of evaluating T(0) at runtime

--- 04_Rays_Camera_Background/Viewport.h
+++ 04_Rays_Camera_Background/Viewport.h
@@ -8,8 +8,8 @@ public:
 
     Viewport(const Point3& cam_pos, const Vec3& look_dir, Float focal_length, Float aspect_ratio){
         
-        u_vec = Vec3(VP_HEIGHT * aspect_ratio, Float(0), Float(0));
-        v_vec = Vec3(Float(0), -VP_HEIGHT, Float(0));  
+        u_vec = Vec3(VP_HEIGHT * aspect_ratio, ZERO, ZERO);
+        v_vec = Vec3(ZERO, -VP_HEIGHT, ZERO);  
         
         Point3 vp_center = cam_pos + focal_length*look_dir;
         top_left = vp_center - u_vec/Float(2) - v_vec/Float(2);
