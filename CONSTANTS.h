80fffaec1c780bf8a20665f77b5420bde2d2dab5
akrami <ramerame123890@gmail.com>
2026-03-11 19:25:14 +0200

    git add useful Constants header

---
+++ CONSTANTS.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Float.h"
+
+// imaging constatnts
+constexpr int DEFAULT_IMAGE_SIZE = 256;
+constexpr int MAX_COLOR = 255;
+constexpr Float DEFAULT_FOCAL_LENGTH = Float(1);
+
